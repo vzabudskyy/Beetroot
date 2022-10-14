03992ebc54ea30235ae298455a2b4913a7b70fc9
Vladislav Zabudskyy <vladzabudskyy@knu.ua>
2022-10-14 23:08:09 +0300

    10/14/2022 upd.1

--- hw_lesson_28/parse_tree_boolean.py
+++ hw_lesson_28/parse_tree_boolean.py
@@ -5,7 +5,6 @@ Modify the `build_parse_tree` and `evaluate functions to
 handle boolean statements (and, or, and not). Remember
 that “not” is a unary operator, so this will somewhat
 complicate your code.
-
 """
 
 
